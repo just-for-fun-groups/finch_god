62e41939af7c7000b7f4cebf24969911fff3768b
黄雪娟 <13477020534@163.com>
2021-11-02 20:32:08 +0800

    userinfo demo

--- finch-god-web-java/src/main/java/com/fitch/god/web/controller/userInfoController.java
+++ finch-god-web-java/src/main/java/com/fitch/god/web/controller/userInfoController.java
@@ -6,11 +6,14 @@ import com.fitch.god.web.service.userInfoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/userInfo")
+@ResponseBody
 public class userInfoController {
 
     @Autowired
